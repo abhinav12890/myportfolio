171924b53662804f116bf558fbb1d6f89ec12b63
Abhinav Kumar <abhinavkumarpal890@gmail.com>
2023-07-25 15:11:25 +0530

    Projects updated commit

--- src/pages/Projects.js
+++ src/pages/Projects.js
@@ -4,7 +4,11 @@ import Layout from "@/components/Layout";
 import Head from "next/head";
 import Image from "next/image";
 import Link from "next/link";
-import project1 from "../../public/images/projects/crypto-screener-cover-image.jpg";
+import airclone from "../../public/images/projects/airclone.png";
+import gclone from "../../public/images/projects/gclone.png";
+import spotifyclone from "../../public/images/projects/spotifyclone.png";
+import twitter from "../../public/images/projects/twitterclone.png";
+import ytclone from "../../public/images/projects/ytclone.png";
 import { motion } from "framer-motion";
 import TransitionEffect from "@/components/TransitionEffect";
 
@@ -80,53 +84,43 @@ const Projects = () => {
                     <div className="grid grid-cols-12 gap-24 gap-y-32 xl:gap-x-16 lg:gap-x-8 md:gap-y-24 sm:gap-x-0">
                         <div className="col-span-12">
                             <FeaturedProject
-                                title="Crypto Screener Application"
-                                img={project1}
+                                title="Airbnb Clone"
+                                img={airclone}
                                 summary="A feature-rich Crypto Screener App using React, Tailwind CSS, Context API, React Router and Recharts. 
                     It shows detail regarding almost all the cryptocurrency. You can easily convert the price in your 
                     local currency."
-                                link="/"
+                                link="https://abhi-airbnb-clone.vercel.app/"
                                 type="Featured Project"
-                                github="/"
+                                github="https://github.com/abhinav12890/airbnb_clone"
                             />
                         </div>
                         <div className="col-span-6 sm:col-span-12"><Project
-                            title="Crypto Screener Application"
-                            img={project1}
-                            link="/"
-                            type="Featured Project"
-                            github="/"
+                            title="Spotify Clone"
+                            img={spotifyclone}
+                            link="https://spotify-clone-9o7qum0nz-abhinav12890.vercel.app/"
+                            type="Project"
+                            github="https://github.com/abhinav12890/spotify-clone"
                         /></div>
                         <div className="col-span-6 sm:col-span-12"><Project
-                            title="Crypto Screener Application"
-                            img={project1}
-                            link="/"
-                            type="Featured Project"
-                            github="/"
-                        /></div>
-                        <div className="col-span-12"><FeaturedProject
-                            title="Crypto Screener Application"
-                            img={project1}
-                            summary="A feature-rich Crypto Screener App using React, Tailwind CSS, Context API, React Router and Recharts. 
-                    It shows detail regarding almost all the cryptocurrency. You can easily convert the price in your 
-                    local currency."
-                            link="/"
-                            type="Featured Project"
-                            github="/"
+                            title="Twitter Clone"
+                            img={twitter}
+                            link="https://twitter-clone-six-henna.vercel.app/"
+                            type="Project"
+                            github="https://github.com/abhinav12890/twitter-clone"
                         /></div>
                         <div className="col-span-6 sm:col-span-12"><Project
-                            title="Crypto Screener Application"
-                            img={project1}
-                            link="/"
-                            type="Featured Project"
-                            github="/"
+                            title="Youtube Clone"
+                            img={ytclone}
+                            link="https://abhi-ytclone.netlify.app/"
+                            type="Project"
+                            github="https://github.com/abhinav12890/ytclone"
                         /></div>
                         <div className="col-span-6 sm:col-span-12"><Project
-                            title="Crypto Screener Application"
-                            img={project1}
-                            link="/"
-                            type="Featured Project"
-                            github="/"
+                            title="Google Clone"
+                            img={gclone}
+                            link="https://abhi-doodleclone.netlify.app/"
+                            type="Project"
+                            github="https://github.com/abhinav12890/googleclone"
                         /></div>
                     </div>
                 </Layout>
